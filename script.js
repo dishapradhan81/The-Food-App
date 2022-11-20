2194c3af411378fc622aa03d9047640392aebc65
dishapradhan81 <dishaquier01@gmail.com>
2022-11-20 20:13:26 +0530

    getProducts,displayProducts,getBagButtons,saveProducts

--- script.js
+++ script.js
@@ -132,21 +132,86 @@ const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".card2");
+const productsDOM = document.querySelector(".card");
 
 // cart
 let cart = [];
 
 // getting the products
 class Products{
+    async getProducts(){
+        try{
+        let result = await fetch('products.json');
+        let data = await result.json();
+
+        let products = data.items;
+        products = products.map(item => {
+            const {title,price} =  item.fields;
+            const {id} = item.sys;
+            const image = item.fields.image.fields.file.url;
+            return {title,price,id,image};
+        });
+        return products;
+        // return data;
+       
+        
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
 
 }
 // display products
 class UI{
+    displayProducts(products){
+        //console.log(products); //we are getting array of products
+
+        let result = '';
+        products.forEach(product => {       //iterating through array of products & dynamically adding italian food items    
+        result += ` 
+        <div class="container-list-item">      
+        <div class="img-container">
+        <img class="list-item-img" src=${product.image}>
+        <button class="bag-button" data-id=${product.id}>
+            <i class="fa-solid fa-cart-shopping"></i>
+            Add to Cart
+        </button>
+        </div>
+        <div class="item-des">
+            <span class="list-item-name">${product.title}</span>
+            <span class="list-item-price">$${product.price}</span>
+        </div>
+
+    </div>`;
+  });
+
+  productsDOM.innerHTML = result;
+
+}
+
+  getBagButtons(){
+    const buttons = [...document.querySelectorAll(".bag-button")];
+    // console.log(buttons);
+
+    buttons.forEach(button => {
+        let id = button.dataset.id;     //if u console.log(buttons) then u will find id under dataset
+        console.log(id);
+
+    })
+    
+   
+
+}
+
+
 
 }
 // local storage
 class Storage{
+    static saveProducts(products){
+        localStorage.setItem("products", JSON.stringify(products));
+    }
 
 }
 
@@ -154,6 +219,18 @@ document.addEventListener("DOMContentLoaded",()=>{
     // instances
     const ui = new UI();
     const products = new Products();
-    
+
+    //get all the products
+   //products.getProducts().then(data => console.log(data));
+   
+   products.getProducts().then(data => {
+    ui.displayProducts(data);
+    Storage.saveProducts(data);
+
+   }).then(()=> {
+    ui.getBagButtons();
+
+   });
+      
 
 });
